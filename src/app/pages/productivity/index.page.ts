eee0a2122301985a53b256ec11cde4ccac7b1dad
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-10-02 21:11:53 +0200

    literal

--- src/app/pages/productivity/index.page.ts
+++ src/app/pages/productivity/index.page.ts
@@ -15,7 +15,7 @@ import {RouterLink} from '@angular/router';
       <p class="mb-7 text-sm">
         This productivity hack taught my brain that distracting
         websites are not available, so I don't even try to open them anymore.
-        So whenever you try to visit these websites, you'll see "This site can't be reached," which is exactly what
+        So whenever you try to visit these websites, you'll see "This site can't be reached", which is exactly what
         I need.
         <br><br>
         By the way, web extensions don't work for me because they're too easy to remove. With this method,
