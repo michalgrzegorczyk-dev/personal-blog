6ce2fe9614ef6899b12f50a2f42b3c8a9fe45e89
Michal Grzegorczyk <michalgrzegorczyk.dev@gmail.com>
2024-10-14 01:18:47 +0200

    refactor

--- src/server/routes/rss.xml.ts
+++ src/server/routes/rss.xml.ts
@@ -1,33 +1,35 @@
 //server/routes/rss.xml.ts
-
+import {injectContentFiles} from '@analogjs/content';
 import { defineEventHandler, setHeader } from 'h3';
-// Simple function to get blog posts (replace with your actual data fetching logic)
-function getBlogPosts() {
-  return [
-    { title: 'First Post', date: '2024-03-01', slug: '/blog/first-post' },
-    { title: 'Second Post', date: '2024-03-15', slug: '/blog/second-post' },
-    // Add more posts as needed
-  ];
-}
+import PostAttributes from "../../app/post-attributes";
 
-export default defineEventHandler((event) => {
-  const posts = getBlogPosts();
-  const siteURL = 'https://yourblog.com'; // Replace with your actual site URL
+// // Simple function to get blog posts (replace with your actual data fetching logic)
+// function getBlogPosts() {
+//   return [
+//     { title: 'First Post', date: '2024-03-01', slug: '/blog/first-post' },
+//     { title: 'Second Post', date: '2024-03-15', slug: '/blog/second-post' },
+//     // Add more posts as needed
+//   ];
+// }
+
+export default defineEventHandler(async (event) => {
+  const posts = injectContentFiles<PostAttributes>();
+
+  const siteURL = 'https://michalgrzegorczyk.dev'; // Replace with your actual site URL
 
   let feedString = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
   <channel>
-    <title>Your Blog Title</title>
+    <title>Personal Blog - Michal Grzegorczyk</title>
     <link>${siteURL}</link>
-    <description>Your blog description</description>
+    <description></description>
     <language>en-us</language>`;
 
   posts.forEach(post => {
     feedString += `
     <item>
-      <title>${post.title}</title>
-      <link>${siteURL}${post.slug}</link>
-      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      <title>${post.attributes.title}</title>
+      <link>${siteURL}${post.attributes.slug}</link>
     </item>`;
   });
 
